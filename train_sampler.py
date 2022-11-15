f83472fa1c90af45f3ac048c466dc012c17b57ec
AlexVerine <alexverine@me.com>
2022-11-15 09:28:02 +0100

    Add Discriminator

---
+++ train_sampler.py
@@ -0,0 +1,179 @@
+from asyncio import DatagramProtocol
+from curses import BUTTON1_PRESSED
+import torch 
+import numpy as np
+import tqdm
+import os
+import argparse
+
+
+from model import Glow, Discriminator
+from utils import AddNoise, get_optimizer, NLLLoss, weights_init_normal
+
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torch.nn import DataParallel
+from torch.optim import lr_scheduler
+
+
+
+def one_step_training(inputs, samples, optimizer, D, criterion):
+    optimizer.zero_grad()
+    inputs = inputs.cuda(non_blocking=True)
+    valid = torch.Tensor(inputs.shape[0], 1).fill_(1.0).cuda()
+    fake = torch.Tensor(inputs.shape[0], 1).fill_(0.0).cuda()
+    Dxr = D(inputs)
+    Dxf = D(samples)
+    real_loss = criterion(Dxr, valid)
+    fake_loss = criterion(Dxf, fake)
+    loss = (real_loss+fake_loss)/2
+    loss.backward()
+    optimizer.step()
+    return loss.detach().cpu().numpy()
+
+def one_step_eval(inputs, samples, D, criterion):
+    inputs = inputs.cuda(non_blocking=True)
+    valid = torch.Tensor(inputs.shape[0], 1).fill_(1.0).cuda()
+    fake = torch.Tensor(inputs.shape[0], 1).fill_(0.0).cuda()
+    Dxr = D(inputs)
+    Dxf = D(samples)
+    real_loss = criterion(Dxr, valid)
+    fake_loss = criterion(Dxf, fake)
+    loss = (real_loss+fake_loss)/2
+    return real_loss.detach().cpu().numpy(), fake_loss.detach().cpu().numpy(), loss.detach().cpu().numpy()
+
+def generate_data(batch_size, model, x_dim):
+    with torch.no_grad():
+        sample = torch.randn(batch_size,
+                          x_dim[1]*x_dim[2],
+                          1,
+                          1).cuda()
+        x, _ = model(sample, None, True)
+    return x
+
+
+def load_model_F(model):
+    state = torch.load(os.path.join('checkpoint', 'bestmodel.pth'))
+    model.load_state_dict(state['model_state_dict'])
+    return model
+
+def save_model_D(model):
+    state = {'model_state_dict': model.state_dict()}
+    torch.save(state, os.path.join('checkpoint', 'bestmodel_D.pth'))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train Normalizing Flow.')
+    parser.add_argument("--epochs", type=int, default=200,
+                        help="Number of epochs for training.")
+    parser.add_argument("--batch_size", type=int, default=64,
+                      help="The batch size to use for training.")
+    parser.add_argument("--optimizer", type=str, default='adam',
+                        help="Optimizer to use for training.")
+    parser.add_argument("--gamma", type=float, default=0.5,
+                      help="Gamma value to use for learning rate schedule.")
+    parser.add_argument("--lr", type=float, default=0.0002,
+                      help="The learning rate to use for training.")
+    parser.add_argument("--wd", type=float, default=0,
+                      help="Weight decay to use for training.")
+    parser.add_argument("--scheduler_decay", type=str, default='100-150',
+                      help="Decay to use for the learning rate schedule.")
+    parser.add_argument("--frequency_log_steps", type=int, default=5,
+                      help="Print log for every step.")
+    parser.add_argument("--padding", type=int, default=0)
+    parser.add_argument("--num_levels", type=int, default=0,
+                      help="Number of downsizing to use in the model.")
+    parser.add_argument("--num_channels", type=int, default=32,
+                      help="Number of channels to use in the model.")
+    parser.add_argument("--num_features", type=int, default=1024)
+    parser.add_argument("--num_steps", type=int, default=15,
+                      help="Depth of the model.")
+    parser.add_argument("--batchnorm", default=False, action='store_false')
+    parser.add_argument("--actnorm", default=True, action='store_true')
+    parser.add_argument("--activation", type=str, default='relu') 
+
+    args = parser.parse_args()
+
+
+
+    # Data Pipeline
+    print('Dataset loading...')
+    x_dim = (1, 32, 32)
+    transform = transforms.Compose([
+                                transforms.Pad(padding=2),
+                                transforms.ToTensor(),
+                                AddNoise()])
+
+    train_dataset = MNIST(root='data/mnist/', 
+                                   train=True, transform=transform, download=True)
+    test_dataset = MNIST(root='data/mnist/', 
+                                  train=False, transform=transform, download=False)
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                               batch_size=args.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                              batch_size=args.batch_size, shuffle=False)
+    print('Dataset Loaded.')
+
+
+    print('Model Loading...')
+    # Model Pipeline
+    x_dim = (1, 32, 32)
+    args.num_levels = int(np.log2(x_dim[1]))-1
+
+
+    print('Model Loading...')
+    # Model Pipeline
+    model = Glow(in_channels = 1,
+                 num_channels = args.num_channels,
+                 num_levels = args.num_levels, 
+                 num_steps = args.num_steps,
+                 params = args).cuda()
+    model = load_model_F(model)
+    model = DataParallel(model).cuda()
+    model.eval()
+
+    D = Discriminator()
+    D = DataParallel(D).cuda()
+    D.apply(weights_init_normal)
+    print('Model loaded.')
+
+    # Optimizer 
+    optimizer = get_optimizer(args, D)
+
+    milestones = list(map(int, args.scheduler_decay.split('-')))
+    scheduler = lr_scheduler.MultiStepLR(
+      optimizer, milestones=milestones, gamma=args.gamma)
+
+
+    # define loss
+    criterion = torch.nn.BCELoss()
+
+
+
+    loss_eval = np.inf
+    best_loss = np.inf
+    with tqdm.trange(args.epochs) as te:
+
+        for epoch in te:
+            te.set_description(f'loss eval: {loss_eval : .2f} \t best loss: {best_loss : .2f}')
+            with tqdm(train_loader, leave=False) as bt:
+                for n_batch, (inputs, label) in enumerate(bt):
+                    samples = generate_data(args.batch_size, model, x_dim)
+                    loss = one_step_training(inputs, samples, optimizer, D, criterion)
+                    bt.set_description(f'Batch {n_batch}, Train {loss: .2f}')
+            with tqdm(test_loader, leave=False) as bte:
+                for n_batch, (inputs, label) in enumerate(bte):
+                    samples = generate_data(args.batch_size, model, x_dim)
+                    real_loss_eval, fake_loss_eval, loss_eval = one_step_eval(inputs, samples, D, criterion)
+                    bte.set_description(f'Batch {n_batch}, Real {real_loss_eval/(n_batch*args.batch_size): .2f}, Fake {fake_loss_eval/(n_batch*args.batch_size): .2f}')
+            loss_eval /= 10000
+            if loss_eval <= best_loss:
+                best_loss = loss_eval
+                save_model_D(D)
+            
+    print('Training done')
+
+        
+
+
--- utils.py
+++ utils.py
@@ -77,6 +77,14 @@ class NLLLoss(nn.Module):
     return nll.mean(), bits_per_dim(z, nll).mean()
   
 
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+
 
 class AddNoise(nn.Module):
   """ add Gaussian or Uniform Noise to the input """
